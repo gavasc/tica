2f2c09a398cefe0b18ebbbcdc417fb7d6a30d815
Gabriel Vasconcelos <gavascdev@gmail.com>
2024-12-10 23:13:24 -0300

    :sparkles: start of commands

---
+++ cmd/punch.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2024 gavasc <gavascdev@gmail.com>
+
+*/
+package cmd
+
+import (
+	"fmt"
+  "time"
+
+	"github.com/spf13/cobra"
+)
+
+// puchCmd represents the puch command
+var punchCmd = &cobra.Command{
+	Use:   "punch",
+	Short: "Punches the current time in a task",
+	Long: `Punches the current time in a task and, if the task does not exist,
+it creates it an starts its timer`,
+	Run: func(cmd *cobra.Command, args []string) {
+    task := args[0]
+    now := time.Now()
+    fmt.Printf("Punching %s - %s\n", task, now.Format("02/01/2006 15:04")) 
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(punchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// puchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// puchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 gavasc <gavascdev@gmail.com>
+
+*/
+package main
+
+import "github.com/gavasc/tica/cmd"
+
+func main() {
+	cmd.Execute()
+}

c3e862cb58198f5fd3712dff4e9e52adcece12a8
Gabriel Vasconcelos <gavascdev@gmail.com>
2025-02-18 15:22:14 -0300

    adjusting basic commands

--- main.go
+++ main.go
@@ -6,24 +6,29 @@ import (
 )
 
 func main() {
-  args := os.Args
-  command := args[1]
+	args := os.Args
+	if len(args) <= 1 {
+		fmt.Println("Missing command!")
+		return
+	}
+	command := args[1]
 
-  switch command {
-    case "punch":
-      if len(args) >= 3 {
-        PunchHandler(args[2:]) //TODO change to pass only the task code string
-      } else {
-        fmt.Println("Missing task code!")
-      }
-    case "list":
-      fmt.Println(ListHandler())
-    case "delete":
-      if len(args) >= 3 {
-        DeleteHandler(args[2])
-      } else {
-        fmt.Println("Missing task code!")
-      }
-  }
+	switch command {
+	case "punch":
+		if len(args) >= 3 {
+			PunchHandler(args[2:]) // TODO change to pass only the task code string
+		} else {
+			fmt.Println("Missing task code!")
+		}
+	case "list":
+		fmt.Println(ListHandler())
+	case "delete":
+		if len(args) >= 3 {
+			DeleteHandler(args[2])
+		} else {
+			fmt.Println("Missing task code!")
+		}
+	default:
+		fmt.Println("Unknown command")
+	}
 }
-
